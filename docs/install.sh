de88326e4ff5dfe7d7713e0d64ffea1ba4175af6
Will Hoover <ugate.relay@gmail.com>
2016-08-01 14:45:57 -0400

    Create install.sh

---
+++ docs/install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# configures and installs the requirements for running node-ugate
+# run 'sudo chmod +x install.sh && ./install.sh' to run the installation
+res=
+hn=ugate
+pwo=chip
+pw=
+ssid=
+ssidp=
+echo -n "Enter the CURRENT password for \"$USER\" [chip] > "
+read -s res
+if [ -n "$res" ]; then
+  pwo=$res
+fi
+echo -n "Enter a NEW password for \"$USER\" > "
+read -s pw
+if [ -z "$pw" ]; then
+  echo "Password required. Exiting install..."
+  exit 1
+fi
+echo -n "Enter a new hostname for CHIP [ugate] > "
+read res
+if [ -n "$res" ]; then
+  hn=$res
+fi
+echo "=============================="
+nmcli con show
+echo "=============================="
+echo -n "Enter a SSID name to connect to from the connections above > "
+read ssid
+if [ -z "$ssid" ]; then
+  echo "SSID required. Exiting install..."
+  exit 1
+fi
+echo -n "Enter the password to connect to $ssid > "
+read -s ssidp
+if [ -z "$ssidp" ]; then
+  echo "SSID password required. Exiting install..."
+  exit 1
+fi
+echo "$pwo" | sudo -S nmcli device wifi connect "$ssid" password "$ssidp" ifname wlan0
